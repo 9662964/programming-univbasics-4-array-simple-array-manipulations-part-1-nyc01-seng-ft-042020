f0c833cf8b1b95fdc23dcdecde84cf4b6f216b2f
ILJOO CHAE <9662964@gmail.com>
2020-04-13 15:50:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+#adds that string to the front of the array using the .unshift method.
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+ array.pop[2]
+
+end
+
+
+def using_shift(array)
+  array.shift
+end
+
+
+def shift_with_args(array)
+  array.splice(-2,2)
+end
